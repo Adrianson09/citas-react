b9049a17a8ccd3539851a5f4c7338174476a9ee9
Adrian Guerrero <adrianson@outlook.com>
2022-07-05 14:44:49 -0600

    Primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = ({children}) => {
+  return (
+    <div className="bg-red-800 text-center text-white font-bold w-full p-3 uppercase mb-3 rounded-md">
+        {children}
+    </div>
+  )
+}
+
+export default Error
